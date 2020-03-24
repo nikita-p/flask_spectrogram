0cb39b6c35d27ed82a9eda16b5b257a3eb6a5fbe
nikita-p <nikita.petrov.1997@gmail.com>
2020-03-24 16:59:40 +0700

    new fix

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, Response, request, redirect, url_for
-import librosa
-import librosa.display
 import numpy as np
+from tempfile import mktemp
+from pydub import AudioSegment
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+import io
+import os
+import base64
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 UPLOAD_FOLDER = "static"
 ALLOWED_EXTENSIONS = set(["mp3", "wav"])
@@ -13,8 +19,31 @@ app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
 
 @app.route("/")
 def hello():
-    y, sr = librosa.load(librosa.util.example_audio_file())
-    return "Hello World!"
+    filename = 'static/example.mp3'
+    mp3_audio = AudioSegment.from_file(filename, format="mp3")
+    wname = mktemp(".wav")
+    mp3_audio.export(wname, format="wav")
+    FS, data = wavfile.read(wname)
+    data = data.mean(axis=1)
+    pngImageB64String = plot_image(data, FS)
+    return render_template("template.html", name=filename, url=pngImageB64String)
+
+
+def plot_image(data, FS):
+    # Generate plot
+    fig = plt.figure()
+    plt.specgram(data, Fs=FS, NFFT=128, noverlap=0)
+    plt.title("Power spectrogram")
+    # plt.colorbar(format="%+2.0f dB")
+    # plt.tight_layout()
+    # Convert plot to PNG image
+    pngImage = io.BytesIO()
+    FigureCanvas(fig).print_png(pngImage)
+
+    # Encode PNG image to base64 string
+    pngImageB64String = "data:image/png;base64,"
+    pngImageB64String += base64.b64encode(pngImage.getvalue()).decode("utf8")
+    return pngImageB64String
 
 
 if __name__ == "__main__":
