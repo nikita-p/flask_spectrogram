a7d143328ef827854abe91affdfa867f53d40b1a
nikita-p <nikita.petrov.1997@gmail.com>
2020-03-24 16:32:19 +0700

    fix5

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, Response, request, redirect, url_for
+import librosa
+import librosa.display
+import numpy as np
 
 UPLOAD_FOLDER = "static"
 ALLOWED_EXTENSIONS = set(["mp3", "wav"])
@@ -10,6 +13,7 @@ app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
 
 @app.route("/")
 def hello():
+    y, sr = librosa.load(librosa.util.example_audio_file())
     return "Hello World!"
 
 
